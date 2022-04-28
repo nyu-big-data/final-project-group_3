725d38bd628a8c2bb7b5991af5ded84ba4ca7d29
Luoyao Chen <lc4866@hlog-1.hpc.nyu.edu>
2022-04-27 21:10:30 -0400

    updates: ALS_MAP and ALS_MSE

--- ALS_model_map.py
+++ ALS_model_map.py
@@ -10,47 +10,62 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 
+def main(spark, netID):
+    train= spark.read.parquet('hdfs:/user/lc4866/train_data.parquet')
+    val= spark.read.parquet('hdfs:/user/lc4866/val_data.parquet')
+    test= spark.read.parquet('hdfs:/user/lc4866/test_data.parquet')
 
-train= spark.read.parquet('train_data.parquet')
-val= spark.read.parquet('val_data.parquet')
-test= spark.read.parquet('test_data.parquet')
+    train.createOrReplaceTempView('train')
+    val.createOrReplaceTempView('val')
+    test.createOrReplaceTempView('test')
 
-train.createOrReplaceTempView('train')
-val.createOrReplaceTempView('val')
-test.createOrReplaceTempView('test')
-
-def als_model(train, rank, reg):
-  als = ALS(rank=rank, maxIter=10, regParam=reg, userCol="userId", itemCol="movieId", ratingCol="rating", coldStartStrategy="drop")
-  model = als.fit(train)
-  return model 
+    def als_model(train, rank, reg):
+      als = ALS(rank=rank, maxIter=10, regParam=reg, userCol="userId", itemCol="movieId", ratingCol="rating", coldStartStrategy="drop")
+      model = als.fit(train)
+      return model 
 
 
 
-regs = [0.01,0.05,0.1,0.2,0.3,0.4,0.5]
-ranks = [10, 20, 30, 40, 50]
+    regs = [0.01,0.05,0.1,0.2,0.3,0.4,0.5]
+    ranks = [10, 20, 30, 40, 50]
 
-best_map=0
-best_rank=0
-best_reg=0
+    best_map=0
+    best_rank=0
+    best_reg=0
 
-for rank in ranks:
-  for reg in regs:
-    als= als_model(train, rank, reg)  
-    val_users = val.select("userId").distinct()
-    val_users_rec = als.recommendForUserSubset(val_users, 100) 
-    pred= val_users_rec.select(val_users_rec['userId'], val_users_rec.recommendations['movieId'].alias('pred'))
-    true = val.orderBy(val['rating'].desc()).groupby('userId').agg(func.collect_set('movieId').alias('true'))
-    predAndtrue = pred.join(true, 'userId').rdd.map(lambda row: (row[1], row[2])) 
+    for rank in ranks:
+      for reg in regs:
+        als= als_model(train, rank, reg)  
+        val_users = val.select("userId").distinct()
+        val_users_rec = als.recommendForUserSubset(val_users, 100) 
+        pred= val_users_rec.select(val_users_rec['userId'], val_users_rec.recommendations['movieId'].alias('pred'))
+        true = val.orderBy(val['rating'].desc()).groupby('userId').agg(func.collect_set('movieId').alias('true'))
+        predAndtrue = pred.join(true, 'userId').rdd.map(lambda row: (row[1], row[2])) 
 
-    val_map = RankingMetrics(predAndtrue).meanAveragePrecisionAt(100)
-    
-    if val_map > best_map:
-        best_map = val_map
-        best_rank = rank
-        best_reg = reg 
+        val_map = RankingMetrics(predAndtrue).meanAveragePrecisionAt(100)
+        
+        if val_map > best_map:
+            best_map = val_map
+            best_rank = rank
+            best_reg = reg 
 
-best_als= als_model(train, best_rank, best_reg)  
-test_users = test.select("userId").distinct()
-test_users_rec = als.recommendForUserSubset(test_users, 100) 
-pred= test_users_rec.select(test_users_rec['userId'], test_users_rec.recommendations['movieId'].alias('pred'))
-true = test.orderBy(test['rating'].desc()).groupby('userId').agg(func.collect_set('movieId').a
+    best_als= als_model(train, best_rank, best_reg)  
+    test_users = test.select("userId").distinct()
+    test_users_rec = als.recommendForUserSubset(test_users, 100) 
+    pred= test_users_rec.select(test_users_rec['userId'], test_users_rec.recommendations['movieId'].alias('pred'))
+    true = test.orderBy(test['rating'].desc()).groupby('userId').agg(func.collect_set('movieId')).a
+
+
+
+
+if __name__ == "__main__":
+
+    # Create the spark session object
+    # spark = SparkSession.builder.appName('part1').getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
+
+    # # Get user netID from the command line
+    netID = getpass.getuser()
+
+    # Call our main routine
+    main(spark, netID)
--- ALS_model_mse.py
+++ ALS_model_mse.py
@@ -1,36 +1,60 @@
+from pyspark.sql import SparkSession
+import getpass
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql import Row
 from tqdm import tqdm
 
 from pyspark.mllib.evaluation import RegressionMetrics
-regs = [0.01]#,0.05,0.1,0.2,0.3,0.4,0.5]
-ranks = [5,10,50,100,200,500]
 
-best_rank=0
-best_reg=0
-MSE_dir ={}
-for rank in tqdm(ranks):
-  for reg in regs:
-    als = ALS(rank=rank, maxIter=10, regParam=reg, 
+
+def main(spark, netID):
+    train_data= spark.read.parquet('hdfs:/user/lc4866/train_data.parquet')
+    val_data= spark.read.parquet('hdfs:/user/lc4866/val_data.parquet')
+    test_data= spark.read.parquet('hdfs:/user/lc4866/test_data.parquet')
+
+    regs = [0.01]#,0.05,0.1,0.2,0.3,0.4,0.5]
+    ranks = [5,10,50,100,200,500]
+
+    best_rank=0
+    best_reg=0
+    MSE_dir ={}
+    for rank in tqdm(ranks):
+      for reg in regs:
+        als = ALS(rank=rank, maxIter=10, regParam=reg, 
+                  userCol="userId", itemCol="movieId", ratingCol="rating", coldStartStrategy="drop")
+        model = als.fit(train_data)
+        predictions = model.transform(val_data)
+        scoreAndLabels = val_data.join(predictions,'userId').rdd.map(lambda tup: (tup[2], tup[5]))
+        metrics = RegressionMetrics(scoreAndLabels)
+        mse = metrics.rootMeanSquaredError
+        MSE_dir[(reg, rank)] = mse
+
+    sorted_dir = {k:v for k, v in sorted(MSE_dir.items(), key=lambda x: x[1], reverse=False)}
+    print(f"best reg, rank = {list(sorted_dir.items())[0][0]}")
+
+    (best_reg, best_rank) = (list(sorted_dir.items())[0][0])
+
+    als = ALS(rank=best_rank, maxIter=10, regParam=best_reg, 
               userCol="userId", itemCol="movieId", ratingCol="rating", coldStartStrategy="drop")
     model = als.fit(train_data)
-    predictions = model.transform(val_data)
-    scoreAndLabels = val_data.join(predictions,'userId').rdd.map(lambda tup: (tup[2], tup[5]))
+    predictions = model.transform(test_data)
+    scoreAndLabels = test_data.join(predictions,'userId').rdd.map(lambda tup: (tup[2], tup[5]))
     metrics = RegressionMetrics(scoreAndLabels)
     mse = metrics.rootMeanSquaredError
-    MSE_dir[(reg, rank)] = mse
+    print(f"using best reg ={best_reg}, best rank = {best_rank}, get test mse = {mse}")
 
-sorted_dir = {k:v for k, v in sorted(MSE_dir.items(), key=lambda x: x[1], reverse=False)}
-print(f"best reg, rank = {list(sorted_dir.items())[0][0]}")
 
-(best_reg, best_rank) = (list(sorted_dir.items())[0][0])
 
-als = ALS(rank=best_rank, maxIter=10, regParam=best_reg, 
-          userCol="userId", itemCol="movieId", ratingCol="rating", coldStartStrategy="drop")
-model = als.fit(train_data)
-predictions = model.transform(test_data)
-scoreAndLabels = test_data.join(predictions,'userId').rdd.map(lambda tup: (tup[2], tup[5]))
-metrics = RegressionMetrics(scoreAndLabels)
-mse = metrics.rootMeanSquaredError
-print(f"using best reg ={best_reg}, best rank = {best_rank}, get test mse = {mse}")
+
+if __name__ == "__main__":
+
+    # Create the spark session object
+    # spark = SparkSession.builder.appName('part1').getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
+
+    # # Get user netID from the command line
+    netID = getpass.getuser()
+
+    # Call our main routine
+    main(spark, netID)
