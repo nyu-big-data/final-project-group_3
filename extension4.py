0e42f731e3672f512e542c6bf0238bf666bdfd14
Luoyao Chen <lc4866@hlog-1.hpc.nyu.edu>
2022-05-16 14:15:24 -0400

    extension4

--- extension4.py
+++ extension4.py
@@ -8,6 +8,7 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.mllib.evaluation import RegressionMetrics
 
 from pyspark.sql.functions import split
+from pyspark.sql.functions import sqrt
 
     
 from pyspark.sql.functions import udf
@@ -15,8 +16,10 @@ from pyspark.sql.types import DoubleType
 
 
 ## UMAP
-import umap.umap_ as umap
-import umap.plot
+# import umap.umap_ as umap
+# from umap import UMAP
+
+# import umap.plot
 from matplotlib import pyplot as plt
 
 def main(spark, netID):
@@ -41,88 +44,109 @@ def main(spark, netID):
     # scoreAndLabels = val_data.join(predictions,'userId').rdd.map(lambda tup: (tup[2], tup[4]))
 
 
-    # ## extension 4.1
-    # print("******* extension 4.1 *******")
-    # predictions.createOrReplaceTempView('predictions')
-    # movies.createOrReplaceTempView("movies")
+    ## extension 4.1
+    print("******* extension 4.1 *******")
+    predictions.createOrReplaceTempView('predictions')
+    movies.createOrReplaceTempView("movies")
 
-    # split_col = split(movies['genres'], "\|")
+    split_col = split(movies['genres'], "\|")
+    
+    ## genre_df: movieId ~ genre
+    genre_df = movies.withColumn('genre', split_col.getItem(0)).select('movieId', 'title', 'genre')
+    genre_df.createOrReplaceTempView('genre_df')
+    print("Number of unique genres: \n ")
+    print(genre_df.select('genre').distinct().count())
+    
+    ## merged_df: per user: his avg rating, avg prediction confidence
+    merged_df = spark.sql('select userId, mean(rating) as avg_rating, mean(prediction) as avg_prediction\
+                      from predictions\
+                      group by userId\
+                      order by userId asc')
+    merged_df.createOrReplaceTempView('merged_df')
+    # merged_df.show(10)
+
+    ## df: per user, genre group, how many ratings
+    df = spark.sql("select userId, tb.genre, count(tb.genre) as cnt \
+                from (genre_df g right join predictions p on g.movieId = p.movieId) tb\
+                group by userId, genre order by userId, count(genre) desc")
+    
+    df.createOrReplaceTempView('df')
+    # df.show(10)
+
+    ## user_df: per user, genre group, add avg_rating and low_prediction property for that user
+    user_df = spark.sql('select m.userId, avg_rating, if(avg_prediction<(select percentile(avg_prediction, 0.5) from merged_df), 1, 0) low_prediction, genre, cnt\
+                      from merged_df m join df d on m.userId=d.userId \
+                      order by userId asc, avg_prediction asc')
+    user_df.createOrReplaceTempView('user_df')
+    user_df.show(10)     
+
+
+    ## t- test staitiscs
+    low_user_df = spark.sql("select * from user_df where low_prediction =1")
+    low_user_df.createOrReplaceTempView('low_user_df')
+
+
+    high_user_df = spark.sql("select * from user_df where low_prediction =0")
+    high_user_df.createOrReplaceTempView('high_user_df')
+
+
+    ## t_test tables
+    low_test_df = spark.sql("select genre, mean(cnt) as xbar_1, count(cnt) as n1, variance(cnt) as s1_sq\
+                        from low_user_df\
+                        group by genre")
+    low_test_df.createOrReplaceTempView('low_test_df')
+    print("low_test_df(5):\n")
+    low_test_df.show(5)
+
+
+    high_test_df = spark.sql("select genre, mean(cnt) as xbar_2, count(cnt) as n2, variance(cnt) as s2_sq\
+                        from high_user_df\
+                        group by genre")
+    high_test_df.createOrReplaceTempView('high_test_df')
+    print("high_test_df(5):\n")
+    high_test_df.show(5)
+
+    t_test_df = spark.sql("select low.genre,  xbar_1, n1, s1_sq, xbar_2, n2, s2_sq, \
+                                (xbar_1-xbar_2)/sqrt( (((n1-1)*s1_sq + (n2-1)*s2_sq)/(n1+n2-2)) * (1/n1+1/n2)) as test_statistic\
+                        from low_test_df low join high_test_df high on low.genre = high.genre")
+    t_test_df.createOrReplaceTempView('t_test_df')
+    print("******* t-test table first 30 rows ******* \n")
+    t_test_df.show(30)
+    
+
+
+    # ## extension 4.2
+    # print("******* extension 4.2 ******* ")
+    # movies.createOrReplaceTempView("genre")
+    # split_col = split(genre['genres'], "\|")
     # # print(split_col)
-    # genre_df = movies.withColumn('genre', split_col.getItem(0)).select('movieId', 'title', 'genre')
-    # genre_df.createOrReplaceTempView('genre_df')
-    # print(f"Number of unique genres: {genre_df.select('genre').distinct().count()}")
+    # genre_df = genre.withColumn('genre', split_col.getItem(0))
+    # # genre_df.show()
 
-    # merged_df = spark.sql('select userId, mean(rating) as avg_rating, mean(prediction) as avg_prediction\
-    #                   from predictions\
-    #                   group by userId\
-    #                   order by userId asc')
-    # # merged_df.show(10)
-    # merged_df.createOrReplaceTempView('merged_df')
+    # concat_df = genre_df.join(model.itemFactors,'id').select('id','title','genre', 'features')#.rdd.map(lambda tup: (tup[2], tup[4]))
 
+    # concat_df.createOrReplaceTempView("concat_df")
+    # grouped = spark.sql("select genre, count(*) as cnt\
+    #                       from concat_df \
+    #                       group by genre\
+    #                       order by cnt desc")
+    # print("the top 20 rows of grouped: ", grouped.show(20))
 
-    # df = spark.sql("select userId, tb.genre, count(tb.genre) as cnt \
-    #             from (genre_df g right join predictions p on g.movieId = p.movieId) tb\
-    #             group by userId, genre order by userId, count(genre) desc")
-    
-    # df.createOrReplaceTempView('df')
-    # # df.show(10)
-    # user_genre = spark.sql('select userId, genre, cnt from \
-    #               (select *, row_number() over(partition by userId order by cnt desc) as rk\
-    #               from df) temp\
-    #           where rk =1')
-    #           # group by userId, genre order by max_cnt')
-    # user_genre.createOrReplaceTempView('user_genre')
-    # # user_genre.show(10)
+    # itemFactors =concat_df.select('features').rdd.flatMap(lambda x: x).collect()
+    # arr = np.reshape(np.array(itemFactors),(len(itemFactors), len(itemFactors[0])))
+    # # print(arr.shape)
+    # # print("type(arr): ", type(arr))
+    # mapper = umap.UMAP()
 
-    # user_df = spark.sql('select m.userId, avg_rating,  if(avg_prediction<0.5, 1, 0) low_prediction, genre, cnt\
-    #                   from merged_df m join df d on m.userId=d.userId \
-    #                   order by userId asc, avg_prediction asc')
-    # user_df.createOrReplaceTempView('user_df')
-    # # user_df.show(10)     
-
-    # t_test= spark.sql("select high.*, low.low_mean, low.low_n, low.low_s2 from (select genre, avg(cnt) as high_mean, count(cnt) as high_n, std(cnt)*std(cnt) as high_s2 from user_df\
-    #                where low_prediction=0 group by genre) high inner join\
-    #                (select genre, avg(cnt) as low_mean, count(cnt) as low_n, std(cnt)*std(cnt) as low_s2 from user_df\
-    #                where low_prediction=1 group by genre) low  on high.genre=low.genre \
-    #                where high_n>0 and low_n>0 and high_mean>0 and low_mean>0 and high_s2>0 and low_s2>0")
-     
-    # mul_udf = udf(lambda high_mean, high_n, high_s2, low_mean, low_n, low_s2:  abs(high_mean- low_mean)/((high_s2/high_n + low_s2/low_n)**0.5) , DoubleType())  
-    # temp = t_test.withColumn("result", mul_udf(t_test['high_mean'], t_test['high_n'],t_test['high_s2'],t_test['low_mean'],t_test['low_n'],t_test['low_s2'])) 
-    
-    # print(f"******* t-test table first 30 rows ******* \n: {temp.show(30)}")
+    # val_t = mapper.fit(arr)
 
 
-    ## extension 4.2
-    print("******* extension 4.2 ******* ")
-    movies.createOrReplaceTempView("genre")
-    split_col = split(genre['genres'], "\|")
-    # print(split_col)
-    genre_df = genre.withColumn('genre', split_col.getItem(0))
-    # genre_df.show()
-
-    concat_df = genre_df.join(model.itemFactors,'id').select('id','title','genre', 'features')#.rdd.map(lambda tup: (tup[2], tup[4]))
-
-    concat_df.createOrReplaceTempView("concat_df")
-    grouped = spark.sql("select genre, count(*) as cnt\
-                          from concat_df \
-                          group by genre\
-                          order by cnt desc")
-    print("the top 20 rows of grouped: ", grouped.show(20))
-
-    itemFactors =concat_df.select('features').rdd.flatMap(lambda x: x).collect()
-    arr = np.reshape(np.array(itemFactors),(len(itemFactors), len(itemFactors[0])))
-    # print(arr.shape)
-    # print("type(arr): ", type(arr))
-    mapper = umap.UMAP()
-    val_t = mapper.fit(arr)
-
-
-    ## plot and save image
-    labels =np.array(concat_df.select('genre').rdd.flatMap(lambda x: x).collect())
-    # plot=umap.plot.points(val_t, labels = labels,')
-    fig = plt.figure()
-    print(f"saving the 2D figure, using the data {using_data}.parquet, figure named: {using_data.png}")
-    umap.plot.points(val_t, labels = labels).get_figure().savefig(f'{using_data}.png')
+    # ## plot and save image
+    # labels =np.array(concat_df.select('genre').rdd.flatMap(lambda x: x).collect())
+    # # plot=umap.plot.points(val_t, labels = labels,')
+    # fig = plt.figure()
+    # print(f"saving the 2D figure, using the data {using_data}.parquet, figure named: {using_data.png}")
+    # umap.plot.points(val_t, labels = labels).get_figure().savefig(f'{using_data}.png')
 
 
 
