faa174101eb08df28a56895e40920a0a5ae721f9
Luoyao Chen <80808548+LuoyaoChen@users.noreply.github.com>
2022-05-17 20:15:41 -0400

    update ext4

--- extension4.py
+++ extension4.py
@@ -33,7 +33,7 @@ def main(spark, netID):
     movies = spark.read.csv("movies-large.csv", header=True, schema = 'movieId int, title STRING, genres STRING')
 
     ##### data we are using to plot the UMAP
-    using_data = test_data
+    using_data = train_data
     rank = 300
     reg = 0.01
     ##### modified above 
@@ -47,74 +47,74 @@ def main(spark, netID):
     # scoreAndLabels = val_data.join(predictions,'userId').rdd.map(lambda tup: (tup[2], tup[4]))
 
 
-    # ## extension 4.1
-    # print("******* extension 4.1 *******")
-    # predictions.createOrReplaceTempView('predictions')
-    # movies.createOrReplaceTempView("movies")
+    ## extension 4.1
+    print("******* extension 4.1 *******")
+    predictions.createOrReplaceTempView('predictions')
+    movies.createOrReplaceTempView("movies")
 
-    # split_col = split(movies['genres'], "\|")
+    split_col = split(movies['genres'], "\|")
     
-    # ## genre_df: movieId ~ genre
-    # genre_df = movies.withColumn('genre', split_col.getItem(0)).select('movieId', 'title', 'genre')
-    # genre_df.createOrReplaceTempView('genre_df')
-    # print("Number of unique genres: \n ")
-    # print(genre_df.select('genre').distinct().count())
+    ## genre_df: movieId ~ genre
+    genre_df = movies.withColumn('genre', split_col.getItem(0)).select('movieId', 'title', 'genre')
+    genre_df.createOrReplaceTempView('genre_df')
+    print("Number of unique genres: \n ")
+    print(genre_df.select('genre').distinct().count())
     
-    # ## merged_df: per user: his avg rating, avg prediction confidence
-    # merged_df = spark.sql('select userId, mean(rating) as avg_rating, mean(prediction) as avg_prediction\
-    #                   from predictions\
-    #                   group by userId\
-    #                   order by userId asc')
-    # merged_df.createOrReplaceTempView('merged_df')
-    # # merged_df.show(10)
+    ## merged_df: per user: his avg rating, avg prediction confidence
+    merged_df = spark.sql('select userId, mean(rating) as avg_rating, mean(prediction) as avg_prediction\
+                      from predictions\
+                      group by userId\
+                      order by userId asc')
+    merged_df.createOrReplaceTempView('merged_df')
+    # merged_df.show(10)
 
-    # ## df: per user, genre group, how many ratings
-    # df = spark.sql("select userId, tb.genre, count(tb.genre) as cnt \
-    #             from (genre_df g right join predictions p on g.movieId = p.movieId) tb\
-    #             group by userId, genre order by userId, count(genre) desc")
+    ## df: per user, genre group, how many ratings
+    df = spark.sql("select userId, tb.genre, count(tb.genre) as cnt \
+                from (genre_df g right join predictions p on g.movieId = p.movieId) tb\
+                group by userId, genre order by userId, count(genre) desc")
     
-    # df.createOrReplaceTempView('df')
-    # # df.show(10)
+    df.createOrReplaceTempView('df')
+    # df.show(10)
 
-    # ## user_df: per user, genre group, add avg_rating and low_prediction property for that user
-    # user_df = spark.sql('select m.userId, avg_rating, if(avg_prediction<(select percentile(avg_prediction, 0.5) from merged_df), 1, 0) low_prediction, genre, cnt\
-    #                   from merged_df m join df d on m.userId=d.userId \
-    #                   order by userId asc, avg_prediction asc')
-    # user_df.createOrReplaceTempView('user_df')
-    # user_df.show(10)     
-
-
-    # ## t- test staitiscs
-    # low_user_df = spark.sql("select * from user_df where low_prediction =1")
-    # low_user_df.createOrReplaceTempView('low_user_df')
+    ## user_df: per user, genre group, add avg_rating and low_prediction property for that user
+    user_df = spark.sql('select m.userId, avg_rating, if(avg_prediction<(select percentile(avg_prediction, 0.5) from merged_df), 1, 0) low_prediction, genre, cnt\
+                      from merged_df m join df d on m.userId=d.userId \
+                      order by userId asc, avg_prediction asc')
+    user_df.createOrReplaceTempView('user_df')
+    user_df.show(10)     
 
 
-    # high_user_df = spark.sql("select * from user_df where low_prediction =0")
-    # high_user_df.createOrReplaceTempView('high_user_df')
+    ## t- test staitiscs
+    low_user_df = spark.sql("select * from user_df where low_prediction =1")
+    low_user_df.createOrReplaceTempView('low_user_df')
 
 
-    # ## t_test tables
-    # low_test_df = spark.sql("select genre, mean(cnt) as xbar_1, count(cnt) as n1, variance(cnt) as s1_sq\
-    #                     from low_user_df\
-    #                     group by genre")
-    # low_test_df.createOrReplaceTempView('low_test_df')
-    # print("low_test_df(5):\n")
-    # low_test_df.show(5)
+    high_user_df = spark.sql("select * from user_df where low_prediction =0")
+    high_user_df.createOrReplaceTempView('high_user_df')
 
 
-    # high_test_df = spark.sql("select genre, mean(cnt) as xbar_2, count(cnt) as n2, variance(cnt) as s2_sq\
-    #                     from high_user_df\
-    #                     group by genre")
-    # high_test_df.createOrReplaceTempView('high_test_df')
-    # print("high_test_df(5):\n")
-    # high_test_df.show(5)
+    ## t_test tables
+    low_test_df = spark.sql("select genre, mean(cnt) as xbar_1, count(cnt) as n1, variance(cnt) as s1_sq\
+                        from low_user_df\
+                        group by genre")
+    low_test_df.createOrReplaceTempView('low_test_df')
+    print("low_test_df(5):\n")
+    low_test_df.show(5)
 
-    # t_test_df = spark.sql("select low.genre,  xbar_1, n1, s1_sq, xbar_2, n2, s2_sq, \
-    #                             (xbar_1-xbar_2)/sqrt( (((n1-1)*s1_sq + (n2-1)*s2_sq)/(n1+n2-2)) * (1/n1+1/n2)) as test_statistic\
-    #                     from low_test_df low join high_test_df high on low.genre = high.genre")
-    # t_test_df.createOrReplaceTempView('t_test_df')
-    # print("******* t-test table first 30 rows ******* \n")
-    # t_test_df.show(30)
+
+    high_test_df = spark.sql("select genre, mean(cnt) as xbar_2, count(cnt) as n2, variance(cnt) as s2_sq\
+                        from high_user_df\
+                        group by genre")
+    high_test_df.createOrReplaceTempView('high_test_df')
+    print("high_test_df(5):\n")
+    high_test_df.show(5)
+
+    t_test_df = spark.sql("select low.genre,  xbar_1, n1, s1_sq, xbar_2, n2, s2_sq, \
+                                (xbar_1-xbar_2)/sqrt( (((n1-1)*s1_sq + (n2-1)*s2_sq)/(n1+n2-2)) * (1/n1+1/n2)) as test_statistic\
+                        from low_test_df low join high_test_df high on low.genre = high.genre")
+    t_test_df.createOrReplaceTempView('t_test_df')
+    print("******* t-test table first 30 rows ******* \n")
+    t_test_df.show(30)
     
 
 
